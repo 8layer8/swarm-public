dfe494106f767c68ebc35258cc6d9c7f21e3276e
8layer8 <brad@ntbrad.com>
2022-01-12 12:16:15 -0500

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,10 @@
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce docker-ce-cli
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker brad
+sudo newgrp docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
