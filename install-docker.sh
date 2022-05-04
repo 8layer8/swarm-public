66afb79baf7a5d5f47154627650dbad739993213
8layer8 <brad@ntbrad.com>
2022-05-03 22:02:25 -0400

    updates for debian 11
    needs some more basic utilites to run from scratch

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,5 @@
+apt -y install git htop mc gnupg2 sudo curl wget
+apt -y install software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
