f3613f01690b6dc46de1dc712ab772984fa95648
8layer8 <brad@ntbrad.com>
2023-08-22 15:07:49 -0400

    Create update.sh

---
+++ docker-compose/plex/update.sh
@@ -0,0 +1,4 @@
+docker service update --force --update-parallelism 1 plex_plex
+docker service update --force --update-parallelism 1 plex_aplexa
+docker service update --force --update-parallelism 1 sctx/overseerr
+docker service update --force --update-parallelism 1 meisnate12/plex-meta-manager
